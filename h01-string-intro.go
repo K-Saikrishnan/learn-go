e3521d2eda53a7173e1c7775e35626d1d3140279
K-Saikrishnan <53394202+K-Saikrishnan@users.noreply.github.com>
2024-03-02 14:52:18 +0530

    ✨h01-string-intro

---
+++ h01-string-intro.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World! 🎉🎉")
+	fmt.Println(`
+	Take this kiss upon the brow!
+	And, in parting from you now,
+	Thus much let me avow —
+	You are not wrong, who deem
+	That my days have been a dream;
+	Yet if hope has flown away
+	In a night, or in a day,
+	In a vision, or in none,
+	Is it therefore the less gone?
+	All that we see or seem
+	Is but a dream within a dream.
+
+	I stand amid the roar
+	Of a surf-tormented shore,
+	And I hold within my hand
+	Grains of the golden sand —
+	How few! yet how they creep
+	Through my fingers to the deep,
+	While I weep — while I weep!
+	O God! Can I not grasp
+	Them with a tighter clasp?
+	O God! can I not save
+	One from the pitiless wave?
+	Is all that we see or seem
+	But a dream within a dream?
+	`)
+}
